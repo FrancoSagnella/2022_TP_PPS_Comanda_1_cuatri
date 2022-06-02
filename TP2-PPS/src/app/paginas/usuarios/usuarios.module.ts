c1309fb1c3699eeb933aec7e77e700820661b832
Sagnella Franco <Fransagn@gmail.com>
2022-06-01 21:15:13 -0300

    hecho login y logout

--- TP2-PPS/src/app/paginas/usuarios/usuarios.module.ts
+++ TP2-PPS/src/app/paginas/usuarios/usuarios.module.ts
@@ -8,10 +8,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { IonicModule } from '@ionic/angular';
 import { ListadoComponent } from './listado/listado.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 
 @NgModule({
-  declarations: [AltaDuenioSupervisorComponent, AltaClienteComponent, ListadoComponent],
+  declarations: [AltaDuenioSupervisorComponent, AltaClienteComponent, ListadoComponent, LoginComponent, LogoutComponent],
   imports: [
     CommonModule,
     UsuariosRoutingModule,
