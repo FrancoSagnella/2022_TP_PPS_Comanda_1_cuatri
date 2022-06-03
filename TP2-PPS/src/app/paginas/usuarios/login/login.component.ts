ba480bd0e1b39b07a67fd0412fdab9502f47ecfb
Sagnella Franco <Fransagn@gmail.com>
2022-06-03 17:41:12 -0300

    cambios (#9)

--- TP2-PPS/src/app/paginas/usuarios/login/login.component.ts
+++ TP2-PPS/src/app/paginas/usuarios/login/login.component.ts
@@ -16,11 +16,12 @@ export class LoginComponent implements OnInit {
   users = [
     { email: "duenio@duenio.com", password: "111111", icon: "😎" },
     { email: "supervisor@supervisor.com", password: "222222", icon: "🧐" },
-    //{ email: "anonimo@anonimo.com", password: "anonimo", icon: "❓" },
-    { email: "metre@metre.com", password: "123456", icon: "💂" },
-    { email: "mozo@mozo.com", password: "444444", icon: "👨‍💼" },
-    { email: "cocinero@cocinero.com", password: "555555", icon: "👨‍🍳" },
-    { email: "bartender@bartender.com", password: "666666", icon: "🍻" },
+    { email: "cliente@cliente.com", password: "222222", icon: "🧐" },
+    { email: "anonimo@anonimo.com", password: "anonimo", icon: "❓" },
+    // { email: "metre@metre.com", password: "123456", icon: "💂" },
+    // { email: "mozo@mozo.com", password: "444444", icon: "👨‍💼" },
+    // { email: "cocinero@cocinero.com", password: "555555", icon: "👨‍🍳" },
+    // { email: "bartender@bartender.com", password: "666666", icon: "🍻" },
   ]
 
   validationUserMessage = {
