1e15a7caf050480e4c7139bb7f92644315b0d316
Sagnella Franco <Fransagn@gmail.com>
2022-06-09 15:25:04 -0300

    listado mesas

--- TP2-PPS/src/app/paginas/mesa/lista/lista.component.ts
+++ TP2-PPS/src/app/paginas/mesa/lista/lista.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { MesaService } from 'src/app/services/mesa.service';
 
 @Component({
   selector: 'app-lista',
@@ -6,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista.component.scss'],
 })
 export class ListaComponent implements OnInit {
+  tables$: Observable<any>;
 
-  constructor() { }
+  kyndSelected;
+  kynds = [
+    { val: 'Comúnes', img: 'assets/images/default.png' },
+    { val: 'VIPs', img: 'assets/images/default.png' },
+    { val: 'Discapacitados', img: 'assets/images/default.png' },
+  ];
 
-  ngOnInit() {}
+
+  constructor(
+    private mesaService: MesaService,
+    private router: Router,
+    public navCtrl: NavController
+  ) { }
+
+  navigateBack(){
+    this.navCtrl.back();
+  }
+
+  ngOnInit() {
+    this.kyndSelected = this.kynds[0];
+    this.getTables(this.kyndSelected.val);
+  }
+
+  setFilter(p) {
+    this.kyndSelected = p;
+    this.getTables(p.val);
+  }
+
+  getTables(filter: string) {
+    switch (filter) {
+      case 'Discapacitados':
+        this.tables$ = this.mesaService.getDiscapacitados();
+        break;
+
+      case 'VIPs':
+        this.tables$ = this.mesaService.getVips();
+        break;
+
+      default:
+        this.tables$ = this.mesaService.getComunes();
+        break;
+    }
+  }
+
+  redirectTo(path: string) {
+    this.router.navigate([path]);
+  }
+
+  setStatus($event, user) {
+    user.estado = $event.target.value;
+    this.mesaService.setOne(user);
+  }
 
 }
